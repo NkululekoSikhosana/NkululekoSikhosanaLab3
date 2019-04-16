b1ae1ead87a3bf40592800a10a6f5662dc3660b4
Nkululeko <1135124@students.wits.ac.za>
2019-04-16 15:06:16 +0200

    Adding element second

--- newFolder/project/index.js
+++ newFolder/project/index.js
@@ -1,10 +1,17 @@
 'use strict'
 
+let secondButton = document.getElementById('myButton')
 let button = document.getElementById('addButton')
 
 button.addEventListener('click', function () {
-    let paragraph = document.createElement('p') // Create <p> element
-    let text = document.createTextNode('This is a student') // Create text node
-    paragraph.appendChild(text) // Append the text to <p>
-    document.body.appendChild(paragraph) // Append <p> to <body>
+  let paragraph = document.createElement('p') // Create <p> element
+  let text = document.createTextNode('This is a student') // Create text node
+  paragraph.appendChild(text) // Append the text to <p>
+  let students = document.getElementById('studentList')
+  students.appendChild(paragraph) // Append <p> to <body>
+}, false)
+
+secondButton.addEventListener('click', function () {
+  let headerElement = document.getElementById('heading')
+  headerElement.innerHTML = 'My New Heading'
 }, false)
